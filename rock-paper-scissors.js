9f24d912065772e0e1efb310561adf6a0583d123
jvlimos <johnvincentlimos@yahoo.com>
2022-05-25 23:27:46 +0800

    Fill .html file and .js file. Need to design with .css and polish earlier commits

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,149 +1,126 @@
-// FUNCTION rPS {
-// 	GET INPUT rock/paper/scissors
+const rockBtn = document.querySelector('#rock-btn');
+const paperBtn = document.querySelector('#paper-btn');
+const scissorsBtn = document.querySelector('#scissors-btn');
+const outcome = document.querySelector('#outcome');
+const points = document.querySelector('#points');
+const finalOutcome = document.querySelector('#final-outcome');
+const playAgainBtn = document.querySelector('#play-again');
+const gameButtons = document.querySelectorAll('.game-button');
 
-// 	FUNCTION getBotMove() {
-// 		botMove = randomNumber from 1-3 wherein 1 = rock, 2 = paper, 3 = scissors
-// 	}
-// 	getBotMove();
+let userInput;
+let rndm;
+let botMove;
 
-// 	rPS2();
-// 	FUNCTION rPS2 {
-		// IF botMove == rock {
-		// 	IF INPUT == paper {u win};
-		// 	ELSE IF INPUT == scissors {u lose};
-		// 	ELSE rPS2();
-		// }
-		
-		// IF botMove == paper {
-		// 	IF INPUT == rock {u lose};
-		// 	ELSE IF INPUT == scissors {u win};
-		// 	ELSE rPS2();
-		// }
-
-		// IF botMove == scissors {
-		// 	IF INPUT == rock {u win};
-		// 	ELSE IF INPUT == paper {u lose};
-		// 	ELSE rPS2();
-		// }
-// 	}
-
-
+rockBtn.addEventListener('click', () => {
+	userInput = 'rock';
+	game();
+});
+paperBtn.addEventListener('click', () => {
+	userInput = 'paper';
+	game();
+});
+scissorsBtn.addEventListener('click', () => {
+	userInput = 'scissors';
+	game();
+});
 
 let userWin = 0;
 let botWin = 0;
+points.textContent = `Human: ${userWin} | Computer: ${botWin}`;
 
-function rockPaperScissors() {
-
-	let botMove = Math.floor(Math.random() * 3) + 1;
-	let userInput = prompt('Rock, paper, scissors?');
-
-	function computerPlay() {
-
-		if (botMove == 1) {
-			botMove = 'rock';
-			console.log(botMove);
-		} else if (botMove == 2) {
-			botMove = 'paper';
-			console.log(botMove);
-		} else {
-			botMove = 'scissors';
-			console.log(botMove);
-		}
-	}
-	computerPlay();
-
-	function getUserInput() {
-
-		if (userInput != ('rock' || 'paper' || 'scissors')) {
-			getUserInput();
-		} else {
-			return userInput;
-		}
-
-		getUserInput();
-	}
+playAgainBtn.addEventListener('click', playAgain);
 
 
-
-	function compareMoves() {
-
-		if (botMove == 'rock') {
-
-			if (userInput == 'paper') {
-				console.log('u win!');
-				console.log('userWin: ', userWin + 1);
-				console.log('botWin: ', botWin);
-				return userWin++;
-			} else if (userInput == 'scissors') {
-				console.log('u lose, nub');
-				console.log('userWin: ', userWin);
-				console.log('botWin: ', botWin + 1);
-				return botWin++;
-			} else {
-				console.log('draw!');
-				console.log('userWin: ', userWin);
-				console.log('botWin: ', botWin);
-			}
-
-		} else if (botMove == 'paper') {
-
-			if (userInput == 'scissors') {
-				console.log('u win!');
-				console.log('userWin: ', userWin + 1);
-				console.log('botWin: ', botWin);
-				return userWin++;
-			} else if (userInput == 'rock') {
-				console.log('u lose, nub');
-				console.log('userWin: ', userWin);
-				console.log('botWin: ', botWin + 1);
-				return botWin++;
-			} else {
-				console.log('draw!');
-				console.log('userWin: ', userWin);
-				console.log('botWin: ', botWin);
-			}
-
-		} else {
-
-			if (userInput == 'rock') {
-				console.log('u win!');
-				console.log('userWin: ', userWin + 1);
-				console.log('botWin: ', botWin);
-				return userWin++;
-			} else if (userInput == 'paper') {
-				console.log('u lose, nub');
-				console.log('userWin: ', userWin);
-				console.log('botWin: ', botWin + 1);
-				return botWin++;
-			} else {
-				console.log('draw!');
-				console.log('userWin: ', userWin);
-				console.log('botWin: ', botWin);
-			}
-
-		}
-	}
-	compareMoves();
-
-}
 
 function game() {
-
-	rockPaperScissors();
-
-	while ((userWin || botWin) <= 5) {
-
-		rockPaperScissors();
-
-		if (userWin == 5) {
-			alert('U WON OVERALL, KING');
-			break;
-		} else if (botWin == 5) {
-			alert('U LOST TO A BOT. SHAME.');
-			break;
-		}
-
-	}
-
+	getRandomNumber();
+	compareMoves(userInput);
 }
-game();
+
+function getRandomNumber() {
+	rndm = Math.floor(Math.random() * 3) + 1; // gets a random number from 1 to 3
+
+	function assignComputerMove() { // assigns every number from 1 to 3 to a player move
+		if (rndm == 1) botMove = 'rock';
+		if (rndm == 2) botMove = 'paper';
+		if (rndm == 3) botMove = 'scissors';
+	}
+	assignComputerMove();
+}
+
+function ifFiveWins() {
+	if (userWin == 5) {
+		finalOutcome.textContent = 'Congrats! You won against the computer!';
+		playAgainBtn.removeAttribute('hidden');
+		gameButtons.forEach(btn => btn.setAttribute('disabled', 'disabled'));
+	} else if (botWin == 5) {
+		finalOutcome.textContent = 'feelsbadman. Better luck next time.';
+		playAgainBtn.removeAttribute('hidden');
+		gameButtons.forEach(btn => btn.setAttribute('disabled', 'disabled'));
+	}
+}
+
+function compareMoves(userInput) {
+
+	if (botMove == 'rock') {
+		if (userInput == 'paper') {
+			userWin++;
+			points.textContent = `Human: ${userWin} | Computer: ${botWin}`;
+			ifFiveWins();
+			return;
+		} else if (userInput == 'scissors') {
+			botWin++;
+			points.textContent = `Human: ${userWin} | Computer: ${botWin}`;
+			ifFiveWins();
+			return;
+		} else if (userInput == 'rock') {
+			points.textContent = `Human: ${userWin} | Computer: ${botWin}`;
+			ifFiveWins();
+			return;
+		}
+	} 
+	
+	else if (botMove == 'paper') {
+		if (userInput == 'scissors') {
+			userWin++;
+			points.textContent = `Human: ${userWin} | Computer: ${botWin}`;
+			ifFiveWins();
+			return;
+		} else if (userInput == 'rock') {
+			botWin++;
+			points.textContent = `Human: ${userWin} | Computer: ${botWin}`;
+			ifFiveWins();
+			return;
+		} else if (userInput == 'paper') {
+			points.textContent = `Human: ${userWin} | Computer: ${botWin}`;
+			ifFiveWins();
+			return;
+		}
+	} 
+	
+	else {
+		if (userInput == 'rock') {
+			userWin++;
+			points.textContent = `Human: ${userWin} | Computer: ${botWin}`;
+			ifFiveWins();
+			return;
+		} else if (userInput == 'paper') {
+			botWin++;
+			points.textContent = `Human: ${userWin} | Computer: ${botWin}`;
+			ifFiveWins();
+			return;
+		} else if (userInput == 'scissors') {
+			points.textContent = `Human: ${userWin} | Computer: ${botWin}`;
+			ifFiveWins();
+			return;
+		}
+	}
+}
+
+function playAgain() {
+	userWin = 0;
+	botWin = 0;
+	playAgainBtn.setAttribute('hidden', 'hidden');
+	finalOutcome.textContent = '';
+	gameButtons.forEach(btn => btn.removeAttribute('disabled'));
+}
