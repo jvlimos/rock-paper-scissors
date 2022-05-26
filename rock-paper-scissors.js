68b3a883efe3dcff547f841ba38960631ec478e6
jvlimos <johnvincentlimos@yahoo.com>
2022-05-26 12:46:24 +0800

    Add background-color and icons

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -2,7 +2,6 @@ const rockBtn = document.querySelector('#rock-btn');
 const paperBtn = document.querySelector('#paper-btn');
 const scissorsBtn = document.querySelector('#scissors-btn');
 const outcome = document.querySelector('#outcome');
-let outcomeText = outcome.textContent;
 const points = document.querySelector('#points');
 const finalOutcome = document.querySelector('#final-outcome');
 const playAgainBtn = document.querySelector('#play-again');
@@ -13,40 +12,7 @@ let rndm;
 let botMove;
 let userWin = 0;
 let botWin = 0;
-
-// switch (true) { // NOT WORKING!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-// 	case botWin == 4:
-// 		outcomeText = 'Come on! You got this!';
-// 		break;
-// 	case userWin == 4:
-// 		outcomeText = 'You\'re so close!';
-// 		break;
-// 	case botWin == 3:
-// 	case botWin == 2:
-// 	case botWin == 1:
-// 		outcomeText = 'Computer won this round.';
-// 		break;
-// 	case userWin == 3:
-// 	case userWin == 2:
-// 	case userWin == 1:
-// 		outcomeText = 'You won this round.';
-// 		break;
-// }
-
-// if (botWin == 4) {outcomeText = 'Come on! You got this!';}
-// else if (userWin == 4) {outcomeText = 'You\'re so close!';}
-// else if (botWin == 3 || botWin == 2 || botWin == 1) {outcomeText = 'Computer won this round.';}
-// else if (userWin == 3 || userWin == 2 || userWin == 1) {outcomeText = 'You won this round.';}
-
-
-const game = () => {
-	getRandomNumber();
-	compareMoves(userInput);
-	console.log(botMove);
-};
-
-const showPoint = () => points.textContent = `Human: ${userWin} | Computer: ${botWin}`;
-showPoint();
+let winner = '';
 
 rockBtn.addEventListener('click', () => {
 	userInput = 'rock';
@@ -65,6 +31,14 @@ playAgainBtn.addEventListener('click', playAgain);
 
 
 
+const game = () => {
+	getRandomNumber();
+	compareMoves(userInput);
+};
+
+const showPoint = () => points.textContent = `Human: ${userWin} | Computer: ${botWin}`;
+showPoint();
+
 function getRandomNumber() {
 	rndm = Math.floor(Math.random() * 3) + 1; // gets a random number from 1 to 3
 
@@ -76,21 +50,24 @@ function getRandomNumber() {
 	assignComputerMove();
 }
 
-function ifFiveWins() {
+function showMessage() {
 	if (userWin == 5) {
-		finalOutcome.textContent = 'SHEEESH! Congrats, you won against the computer!';
-		playAgainBtn.removeAttribute('hidden');
+		finalOutcome.textContent = 'You won against the computer! Well played.';
+		playAgainBtn.removeAttribute('style', 'visibility: hidden;');
 		gameButtons.forEach(btn => btn.setAttribute('disabled', 'disabled'));
+		outcome.textContent = '';
 	} else if (botWin == 5) {
-		finalOutcome.textContent = 'feelsbadman. Better luck next time.';
-		playAgainBtn.removeAttribute('hidden');
+		finalOutcome.textContent = 'You lost to the computer. Better luck next time!';
+		playAgainBtn.removeAttribute('style', 'visibility: hidden;');
 		gameButtons.forEach(btn => btn.setAttribute('disabled', 'disabled'));
+		outcome.textContent = '';
 	} 
 	
-	// else if (botWin == 4) {outcome.textContent = 'Come on! You got this!';}
-	// else if (userWin == 4) {outcome.textContent = 'You\'re so close!';}
-	// else if (botWin == 3 || botWin == 2 || botWin == 1) {outcome.textContent = 'Computer won this round.';}
-	// else if (userWin == 3 || userWin == 2 || userWin == 1) {outcome.textContent = 'You won this round.';}
+	if (botWin == 4 && winner == 'computer') {outcome.textContent = 'Come on! You got this!';}
+	else if (userWin == 4 && winner == 'human') {outcome.textContent = 'You\'re so close!';}
+	else if ((botWin == 3 || botWin == 2 || botWin == 1) && winner == 'computer') {outcome.textContent = 'Computer won this round.';}
+	else if ((userWin == 3 || userWin == 2 || userWin == 1) && winner == 'human') {outcome.textContent = 'You won this round.';}
+	else if (winner == 'tie') {outcome.textContent = 'Tie.';}
 }
 
 function compareMoves(userInput) {
@@ -98,17 +75,20 @@ function compareMoves(userInput) {
 	if (botMove == 'rock') {
 		if (userInput == 'paper') {
 			userWin++;
+			winner = 'human';
 			showPoint();
-			ifFiveWins();
+			showMessage();
 			return;
 		} else if (userInput == 'scissors') {
 			botWin++;
+			winner = 'computer';
 			showPoint();
-			ifFiveWins();
+			showMessage();
 			return;
 		} else if (userInput == 'rock') {
+			winner = 'tie';
 			showPoint();
-			ifFiveWins();
+			showMessage();
 			return;
 		}
 	} 
@@ -116,17 +96,20 @@ function compareMoves(userInput) {
 	else if (botMove == 'paper') {
 		if (userInput == 'scissors') {
 			userWin++;
+			winner = 'human';
 			showPoint();
-			ifFiveWins();
+			showMessage();
 			return;
 		} else if (userInput == 'rock') {
 			botWin++;
+			winner = 'computer';
 			showPoint();
-			ifFiveWins();
+			showMessage();
 			return;
 		} else if (userInput == 'paper') {
+			winner = 'tie';
 			showPoint();
-			ifFiveWins();
+			showMessage();
 			return;
 		}
 	} 
@@ -134,17 +117,20 @@ function compareMoves(userInput) {
 	else {
 		if (userInput == 'rock') {
 			userWin++;
+			winner = 'human';
 			showPoint();
-			ifFiveWins();
+			showMessage();
 			return;
 		} else if (userInput == 'paper') {
 			botWin++;
+			winner = 'computer';
 			showPoint();
-			ifFiveWins();
+			showMessage();
 			return;
 		} else if (userInput == 'scissors') {
+			winner = 'tie';
 			showPoint();
-			ifFiveWins();
+			showMessage();
 			return;
 		}
 	}
@@ -154,7 +140,7 @@ function playAgain() {
 	userWin = 0;
 	botWin = 0;
 	showPoint();
-	playAgainBtn.setAttribute('hidden', 'hidden');
+	playAgainBtn.setAttribute('style', 'visibility: hidden;');
 	finalOutcome.textContent = '';
 	gameButtons.forEach(btn => btn.removeAttribute('disabled'));
 }
